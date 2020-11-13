f40699eed16fe7d52bdc5c7ccf0d6086ed66c687
Bao Nguyen <bao.nguyen201275@gmail.com>
2020-11-13 15:41:59 +0700

    working on react context api

---
+++ src/Subtotal.js
@@ -0,0 +1,31 @@
+import "./Subtotal.css";
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small class="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button>Proceed to checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNKz7yt9e--WiB1bD9FHRQZyiF87AETIE",
+  authDomain: "challenge-231a3.firebaseapp.com",
+  databaseURL: "https://challenge-231a3.firebaseio.com",
+  projectId: "challenge-231a3",
+  storageBucket: "challenge-231a3.appspot.com",
+  messagingSenderId: "156664857077",
+  appId: "1:156664857077:web:01fe58336e329c443260bc",
+  measurementId: "G-CBKH9BDBLC"
+};
