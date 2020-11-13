a7d99e80694a2a73560503a152226855098b572b
Bao Nguyen <bao.nguyen201275@gmail.com>
2020-11-13 23:06:59 +0700

    working on reducer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, ratting }) {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log(`This is the basket >>> ${basket}`);
+
+  const addToBasket = () => {
+    // Dispatch the item into data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: { id, title, image, price, ratting },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -22,7 +34,7 @@ function Product({ id, title, image, price, ratting }) {
 
       <img src={image} alt="" />
 
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   );
 }
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -4,11 +4,11 @@ import React, { createContext, useContext, useReducer } from "react";
 export const StateContext = createContext();
 
 // Wrap our app and provide data layer
-export const StateProvider = ({ reducer, initialState, children }) => {
+export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
-  </StateContext.Provider>;
-};
+  </StateContext.Provider>
+);
 
 // Pull info from the data layer
 export const useStateValue = () => useContext(StateContext);
